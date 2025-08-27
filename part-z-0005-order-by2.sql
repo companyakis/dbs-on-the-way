23b48cb9fbcffe0459c72a654a17e91a5e0932ef
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-08-27 10:20:33 +0300

    Create part-z-0005-order-by2.sql

---
+++ part-z-0005-order-by2.sql
@@ -0,0 +1,13 @@
+select location_id, postal_code, city
+
+from locations
+
+order by city asc -- asc is optional
+
+-- 2400		"London"
+-- 2700	"80925"	"Munich"
+-- 2500	"OX9 9ZB"	"Oxford"
+-- 1700	"98199"	"Seattle"
+-- 1500	"99236"	"South San Francisco"
+-- 1400	"26192"	"Southlake"
+-- 1800	"M5V 2L7"	"Toronto"
